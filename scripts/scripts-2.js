ce6031130638e8b99503411b814f5313d0a42945
Luxapodular <luca.damasco@gmail.com>
2018-04-09 09:01:17 -0400

    Updating test item

--- scripts/scripts-2.js
+++ scripts/scripts-2.js
@@ -1,4 +1,12 @@
 function getStudentsAlternate() {
   var students = $("textarea#students").val();
-  console.log(students.split(",")); 
+
+  var studentList = students.split(","); 
+  
+  for (var i=0; i<studentList.length; i++) {
+    var trimmedStudent = studentList[i].trim();
+    console.log(trimmedStudent);    
+  }
 }
+
+ 
