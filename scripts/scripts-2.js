0219fef542aba7f3199675d90ca40295244af9c7
Roger Strang <roger.e.strang@gmail.com>
2018-04-11 17:21:20 -0400

    Started to add functionality to button in student section

--- scripts/scripts-2.js
+++ scripts/scripts-2.js
@@ -9,4 +9,32 @@ function getStudentsAlternate() {
   }
 }
 
+function addStudent() {
+   console.log("hello")
+   var nameInput = document.getElementById("studentNameInput");
+    var nameInputValue = nameInput.value;
+   console.log(nameInputValue);
+   var studentLexileInput = document.getElementById("studentLexileInput");
+    var studentLexileValue = studentLexileInput.value;
+    console.log(studentLexileValue);
+    myDiv=document.getElementById("student-list");
+    myDiv.innerHTML=nameInputValue+": "+studentLexileValue
+    
+    //create new student name input box
+    var newStudentNameInput = document.createElement("input");
+    newStudentNameInput.setAttribute("type", "text");
+    newStudentNameInput.setAttribute("placeholder", "Name");
+    var studentInputBoxes = document.getElementById("studentInputBoxes")
+    studentInputBoxes.appendChild(document.createElement("br"));
+    $("#studentInputBoxes").append(newStudentNameInput);
+    
+    //create new student lexile input box
+    var newStudentLexileInput = document.createElement("input");
+    newStudentLexileInput.setAttribute("type", "text");
+    newStudentLexileInput.setAttribute("placeholder", "Lexile");
+    $("#studentInputBoxes").append(newStudentLexileInput);
+    
+    
+
+}
  
