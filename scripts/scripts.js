63019e51e6cd87e6bc50dd12d0873d4624ec0850
Luxapodular <luca.damasco@gmail.com>
2018-04-12 09:35:27 -0400

    Added content updating to the Subgroups

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,33 @@
+var numSubgroups = 1;
+
 function getStudents() {
   var students = $("textarea#students").val();
   console.log(students.split(",")); 
+}
+
+function addContent(obj) {
+  callerId = obj.id; 
+
+  nameId = callerId.replace("content-add-button", "content-name-input");
+  levelId = callerId.replace("content-add-button", "content-level-input");
+  containerId = callerId.replace("content-add-button", "content-container");
+  
+  
+  var nameInput = document.getElementById(nameId); 
+  var levelInput = document.getElementById(levelId);
+
+  var name = $("#"+nameId).val(); 
+  var level = $("#"+levelId).val(); 
+  
+  var contentDiv = document.createElement('div'); 
+  contentDiv.className = "content-div"; 
+  contentDiv.innerHTML = name + ":" + level; 
+  
+  var delButton = document.createElement('button'); 
+  delButton.className="content-delete-button";
+  contentDiv.appendChild(delButton); 
+  
+  $("#"+containerId).append(contentDiv); 
+
+  console.log(name+":"+level); 
 }
